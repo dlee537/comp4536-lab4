530efb2bbd069104f4a8fc8b8ae837730b012930
Daniel <dancheuklee@gmail.com>
2025-10-08 17:26:10 -0700

    Configure cors

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ let words = [
 ];
 
 const server = http.createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
     const parsedURL = url.parse(req.url, true);
     const path = parsedURL.pathname;
     const method = req.method;
